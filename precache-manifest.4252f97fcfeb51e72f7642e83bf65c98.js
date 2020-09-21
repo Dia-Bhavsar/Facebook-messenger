b96c16db40ad5f3f5b85f05378d4b8f268c32500
dia-bhavsar <diabhavsar16@gmail.com>
2020-09-21 23:05:35 +0530

    Updates

---
+++ precache-manifest.4252f97fcfeb51e72f7642e83bf65c98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc6abee91633b802c29db6865414991",
+    "url": "/Facebook-messenger/index.html"
+  },
+  {
+    "revision": "0789f5544df7f8f3c6e1",
+    "url": "/Facebook-messenger/static/css/main.1c719e24.chunk.css"
+  },
+  {
+    "revision": "383f345c07755964f4b3",
+    "url": "/Facebook-messenger/static/js/2.4dade7d7.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Facebook-messenger/static/js/2.4dade7d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0789f5544df7f8f3c6e1",
+    "url": "/Facebook-messenger/static/js/main.33ba7536.chunk.js"
+  },
+  {
+    "revision": "2f3429f104a48060a335",
+    "url": "/Facebook-messenger/static/js/runtime-main.a2c3c64a.js"
+  }
+]);
